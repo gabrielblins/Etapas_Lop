402d601f5477249294c2f1d64f11747c0a5f421d
Gabriel Barros Lins <43868756+gabrielblins@users.noreply.github.com>
2018-11-24 20:39:16 -0300

    Update etapa7.js

--- etapa7.js
+++ etapa7.js
@@ -1,44 +1,55 @@
-var x = 25;
-var y = 575;
-var r = 575;
-var s = 575;
-var xd = 0;
-var yd = 0;
-var disparo = false;
+var x = 0;
+var y = 0;
+var r = [];
+var s = [];
+var x1 = [];
+var y1 = [];
+var disp1 = [];
 var vidas = 5;
 var pontos = 0;
-var colisaod = false;
-var colisaoj = false;
+var colisaod1 = [];
+var colisaoj = [];
 var nivel = 1;
 var t = 0;
 var tempo;
 var bonus = false;
 var xb = -50;
 var yb = -50;
-var velocidade = 5;
+var tipobonus = 0;
+var qtd = 5;
+var velocidade = 0;
+var velocniv = 0;
+var fast = 0;
+
 
 function setup() {
 
 	createCanvas(600,600); 
     background(0);
     frameRate(60);
+    y = random(25,575);
+    for(i=1;i<=qtd;i++){
+        r[i] = random(600,1000);
+        s[i] = random(25,575);
+        colisaod1[i] = false;
+        colisaoj[i] = false;
+        disp1[i] = false;
+        x1[i] = 0;
+        y1[i] = 700;
+    }
     
 }
-function draw() {      
+function draw() {
+   velocidade = velocniv+fast;      
    rectMode(CENTER);
    t++
    tempo = Math.floor(t/60);
    //primeira etapa 
     background(0);
-	if(disparo){
-		stroke(0,255,0);//Muda a cor da elipse quando o disparo eh efetuado
-	}else{
-	stroke(255,255,255);
-	}
+    stroke('white');
 	noFill();
 	ellipse(x,y,50,50); 
-	stroke(255,0,0);
-    rect(r,s,50,50); //objetos em lados opostos. Elipse o jogador, e retângulo o obstáculo.						
+	//objetos em lados opostos. Elipse o jogador, e retângulo o obstáculo.
     
     //Segunda etapa.
 	//elipse se move com comandos do teclado.	
@@ -67,29 +78,7 @@ function draw() {
   		    y+=velocidade;
   	    }
     }
-   
-    //Terceira etapa. Objeto caminha pela tela até desaparecer e retorna posteriormente.
-		r-=5;
-	if(r<-25){ 
-		r = random(650, 1500);
-		s = random (25,575);
-		vidas--;
-    }
-    
-    //Quarta etapa. Disparo eh efetuado quando pressiona a tecla CTRL
-	if (keyIsDown(CONTROL) && !disparo){
-		xd = x;
-		yd = y;
-		disparo = true;
-	}
-	if(disparo == true){
-		stroke(255,255,0);
-		rect(xd,yd,10,10);
-		xd+=5
-	    if(xd > 600){
-		    disparo = false;
- 		}
-	}
+	
 	//Quinta etapa. Informacoes na tela
 	textSize(24);
 	noStroke();
@@ -98,61 +87,99 @@ function draw() {
 	text('Pontos: '+ pontos, 450, 30);
 	text('Nivel: ' + nivel, 250, 30);
 
-	//Sexta etapa. Colisao
 
-	if(dist(x,y,r,s) < 50) {
 
-		if(colisaoj==false){
-			
-			colisaoj = true;
-			vidas--;
-			r = random(650,1000);
-			s = random(25,575);
+    //Setima etapa. numero arbitrario de objetos
+    for(i = 1;i<=qtd;i++){
+        stroke(255,0,0);
+        noFill();
+        rect(r[i],s[i],50,50); 	
+        //Terceira etapa. Objeto caminha pela tela até desaparecer e retorna posteriormente.
+        if(nivel == 1 || nivel==2){
+        r[i] -= 5;
+        }else if(nivel==3 || nivel==4){
+            r[i]-=4;
+        }else{
+            r[i]-=3;
+        }
+        if(r[i]<-25){ 
+            r[i] = random(650, 1500);
+            s[i] = random (25,575);
+            vidas--;
+        }
+        //Sexta etapa. Colisao
+        if(dist(x,y,r[i],s[i]) < 50) {
+            if(colisaoj[i]==false){	
+                colisaoj[i] = true;
+                pontos++;
+                r[i] = random(650,1000);
+                s[i] = random(25,575);
+            }
+        } else {
+            colisaoj[i] = false;
+        }
+        for(k=1;k<=qtd;k++){
+        if(dist(x1[k],y1[k],r[i],s[i]) < 30) {
+            if(!colisaod1[i]) {
+                colisaod1[i] = true;
+                pontos++;
+                r[i] = random(650,1000);
+                s[i] = random(25,575);
+                disp1[k] = false;
+                x1[k] = 0;
+                y1[k] = 700;
+            } 
+        } else { 
+            colisaod1[i] = false;
+        }
+     }
+    }
 
-		}
-		
-
-	} else {
-
-		colisaoj = false;
-	}
-
-	
-	if(dist(xd,yd,r,s) < 30) {
-
-		if(!colisaod) {
-
-			colisaod = true;
-			pontos++;
-			r = random(650,1000);
-			s = random(25,575);
-
-			disparo = false;
-			xd = 0;
-			yd = 700;
-
-		} 
-	} else { 
-
-		colisaod = false;
-		
-	}
-
-	//Setima etapa. 
-	if(t == 300){
+     //Quarta etapa. Disparo eh efetuado quando pressiona a tecla CTRL
+      for(k=1;k<=qtd;k++){
+        if (keyIsDown(32) && !disp1[k]){
+            x1[k] = x;
+            y1[k] = y;
+            disp1[k] = true;
+        }
+        if(disp1[k]){
+            stroke(255,255,0);
+            rect(x1[k],y1[k],10,10);
+            x1[k]+=7
+            if(x1[k] > 600){
+                disp1[k] = false;
+             }
+        }
+    }
+	//Bonus de velocidade/vida    
+	if(t == 600){
 		bonus = true;
 		xb = random(0,600);
-		yb = random(0,600);
+        yb = random(0,600);
+        tipobonus = random(1,100);
 	}
-
+    
 	if(bonus == true){
-	
-		fill(0,0,240);
+	if(tipobonus <=50){
+        fill(0,0,240);
+        noStroke();
 		ellipse(xb,yb,20,20);
 		if (dist(x,y,xb,yb)<45) {
-			velocidade = 8;
-			bonus = false;
-		}
-	}
-
+            fast = 3;
+            bonus = false;
+            t = 0;   
+        }
+    }else{
+        fill(228,120,51);
+        noStroke();
+		ellipse(xb,yb,20,20);
+		if (dist(x,y,xb,yb)<45) {
+			vidas++;
+            bonus = false;
+            t = 0;   
+        }
+    }
+	}if(t == 300){
+        fast = 0;
+    }
 }
